d070c5f5abfb7863522660cc0ace6bd37bccdd43
Cesar San Lucas <csanlucas@live.com>
2022-01-27 00:33:32 -0500

    Add second requirement

--- locator/serializers.py
+++ locator/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
+from django.utils import timezone
 
-from .models import HoumerLocation
+from .models import HoumerLocation, Property
 
 class HoumerLocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = HoumerLocation
         fields = '__all__'
+
+
+class PropertySerializer(serializers.ModelSerializer):
+    elapsed_time_ms = serializers.IntegerField(read_only=True)
+    elapsed_time_str = serializers.CharField(read_only=True)
+
+    def validate_departure_at(self, value):
+        now = timezone.now()
+        if value <= now:
+            raise serializers.ValidationError('Departure datetime is less than arrive time')
+        return value
+
+    class Meta:
+        model = Property
+        fields = ('visited_by', 'arrive_at', 'departure_at', 'latitude', 'longitude', 'elapsed_time_ms', 'elapsed_time_str')
--- locator/views.py
+++ locator/views.py
@@ -1,14 +1,43 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.pagination import LimitOffsetPagination
+import datetime as dt
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
-from .serializers import HoumerLocationSerializer
-from .models import HoumerLocation
+from .serializers import HoumerLocationSerializer, PropertySerializer
+from .models import HoumerLocation, Houmer, Property
 
 
 class HoumerLocationViewSet(APIView):
     def post(self, request, format=None):
         validator = HoumerLocationSerializer(data=request.data)
+        validator.is_valid(raise_exception=True)
+        validator.save()
+        return Response(data=validator.data, status=status.HTTP_201_CREATED)
+
+
+class PropertyViewSet(APIView):
+    def get(self, request):
+        if 'houmer_id' not in request.query_params:
+            return Response({'message': 'Provide query_params [houmer_id] query params'}, status=status.HTTP_400_BAD_REQUEST)
+        if 'on_date' not in request.query_params:
+            return Response({'message': 'Provide query_params [on_date] to query property metrics'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            query_date = dt.datetime.fromisoformat(request.query_params['on_date'])
+            query_date = timezone.get_current_timezone().localize(query_date)
+        except ValueError:
+            return Response({'message': 'Check on date params must be in format YYYY-MM-DD'}, status=status.HTTP_400_BAD_REQUEST)
+        houmer = get_object_or_404(Houmer, pk=request.query_params['houmer_id'])
+        properties_visited = houmer.properties_visited.filter(arrive_at__gt=query_date,
+                                                              departure_at__lt=query_date.replace(hour=23, minute=59, second=59))
+        prop_serializer = PropertySerializer(instance=properties_visited, many=True) 
+        return Response(data=prop_serializer.data)
+
+
+    def post(self, request):
+        validator = PropertySerializer(data=request.data)
         validator.is_valid(raise_exception=True)
         validator.save()
         return Response(data=validator.data, status=status.HTTP_201_CREATED)
