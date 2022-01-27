216f631f7b78aa40195eb50a160fcd330764034e
csanlucas <csanlucas@live.com>
2022-01-27 00:35:35 -0500

    [H-CC2]: Metricas para Bienes inmuebles visitados (#2)
    * Add second requirement
    
    * Add second requirement
    
    Co-authored-by: Cesar San Lucas <slace.93@gmail.com>

--- locator/models.py
+++ locator/models.py
@@ -1,9 +1,32 @@
 from django.db import models
+import datetime as dt
+
+class Houmer(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+
 
 class HoumerLocation(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
-    houmerId = models.CharField(max_length=50)
+    houmerId = models.ForeignKey(Houmer, on_delete=models.CASCADE, related_name='locations')
     deviceId = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=True)
 
+
+class Property(models.Model):
+    visited_by = models.ForeignKey(Houmer, on_delete=models.CASCADE, related_name='properties_visited')
+    arrive_at = models.DateTimeField(auto_now=True)
+    departure_at = models.DateTimeField(blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    @property
+    def elapsed_time_ms(self):
+        elapsed_time = self.departure_at - self.arrive_at
+        return int(elapsed_time.total_seconds() * 1000)
+    
+    @property
+    def elapsed_time_str(self):
+        elapsed_time = self.departure_at - self.arrive_at
+        return dt.timedelta(seconds=elapsed_time.total_seconds())
