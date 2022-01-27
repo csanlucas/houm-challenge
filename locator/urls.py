b0593a97fea12f5d1ef5501ce7adb777a255259b
Cesar San Lucas <slace.93@gmail.com>
2022-01-27 00:25:30 -0500

    Add second requirement

--- locator/urls.py
+++ locator/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HoumerLocationViewSet
+from .views import HoumerLocationViewSet, PropertyViewSet
 
 url_locator = [
-    path('locator/houmer/', HoumerLocationViewSet.as_view())
+    path('locator/houmer/', HoumerLocationViewSet.as_view()),
+    path('locator/property/', PropertyViewSet.as_view())
 ]
